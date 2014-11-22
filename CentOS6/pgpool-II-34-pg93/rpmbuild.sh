ebef15467af4b66acd1d77b58c501b09bca0ce70
Tatsuo Ishii <ishii@postgresql.org>
2014-11-22 17:12:33 +0900

    For pgpool-II 3.4-stable tree allow to merge the changes of HEAD.

--- CentOS6/pgpool-II-34-pg93/rpmbuild.sh
+++ CentOS6/pgpool-II-34-pg93/rpmbuild.sh
@@ -1,20 +1,36 @@
 #! /bin/sh
 # Script to create pgpool-II and PostgreSQL RPMs
 # Run this script as user "postgres".
-
-wget http://www.pgpool.net/download.php?f=pgpool-II-$PGPOOL_VERSION.tar.gz -O /tmp/pgpool-II-$PGPOOL_VERSION.tar.gz
-cd /tmp
-tar xfz pgpool-II-$PGPOOL_VERSION.tar.gz
+PGPOOL_VERSION=${PGPOOL_VERSION:-"3.4.0"}
+PGPOOL_BRANCH=${PGPOOL_BRANCH:-"V3_4_STABLE"}
+POSTGRESQL_VERSION=9.3
+POSTGRESQL_VERSION2=93
 cd /var/lib/pgsql
 mkdir rpmbuild
 cd rpmbuild
 mkdir BUILD BUILDROOT RPMS SOURCES SPECS SRPMS
-cp /tmp/pgpool-II-$PGPOOL_VERSION.tar.gz SOURCES
-cd /tmp
-cd pgpool-II-$PGPOOL_VERSION
-cp src/pgpool.spec /var/lib/pgsql/rpmbuild/SPECS
-cp src/redhat/pgpool.conf.sample.patch /var/lib/pgsql/rpmbuild/SOURCES
-cp src/redhat/pgpool.init /var/lib/pgsql/rpmbuild/SOURCES
-cp src/redhat/pgpool.sysconfig /var/lib/pgsql/rpmbuild/SOURCES
-cd /var/lib/pgsql/rpmbuild/SPECS
-rpmbuild -ba pgpool.spec --define="pgpool_version $PGPOOL_VERSION" --define="pg_version $POSTGRESQL_VERSION2" --define="pghome /usr/pgsql-$POSTGRESQL_VERSION" --define="dist .pgdg"
+cd ..
+cp /tmp/pgpool-II-$PGPOOL_VERSION.tar.gz rpmbuild/SOURCES
+
+cd pgpool2
+git pull
+git checkout $PGPOOL_BRANCH
+git pull
+
+cp src/pgpool.spec ../rpmbuild/SPECS
+cp src/redhat/pgpool.conf.sample.patch ../rpmbuild/SOURCES
+cp src/redhat/pgpool.init ../rpmbuild/SOURCES
+cp src/redhat/pgpool.sysconfig ../rpmbuild/SOURCES
+./configure --with-pgsql=/usr/pgsql-$POSTGRESQL_VERSION
+make
+make dist
+mkdir /tmp/head
+cp pgpool-II-$PGPOOL_VERSION.tar.gz /tmp/head
+cd /tmp/head
+tar xfz pgpool-II-$PGPOOL_VERSION.tar.gz
+cd ..
+diff -crN pgpool-II-$PGPOOL_VERSION head/pgpool-II-$PGPOOL_VERSION > pgpool-II-head.patch
+cd /var/lib/pgsql
+cp /tmp/pgpool-II-head.patch rpmbuild/SOURCES
+cd rpmbuild/SPECS
+rpmbuild -ba pgpool.spec --define="pgpool_version $PGPOOL_VERSION" --define="pg_version $POSTGRESQL_VERSION2" --define="pghome /usr/pgsql-$POSTGRESQL_VERSION" --define="dist .rhel6"
