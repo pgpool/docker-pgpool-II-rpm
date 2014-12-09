85f8ec18064f18938278ef311f172f7ee1322e1c
Tatsuo Ishii <ishii@postgresql.org>
2014-12-09 16:44:32 +0900

    Adjust Docker file and rpmbuild.sh for new rpm name.
    Also restructure Dockerfile rpmbuild.sh. Now
    pgpool-II-33-pg92/rpmbuild.sh and pgpool-II-33-pg93/rpmbuild.sh are
    identical. Also pgpool-II-33-pg92/Dockerfile and
    pgpool-II-33-pg93/Dockerfile are almost same except the version number
    ENV command.

--- CentOS6/pgpool-II-33-pg92/rpmbuild.sh
+++ CentOS6/pgpool-II-33-pg92/rpmbuild.sh
@@ -1,10 +1,7 @@
 #! /bin/sh
 # Script to create pgpool-II and PostgreSQL RPMs
 # Run this script as user "postgres".
-PGPOOL_VERSION=${PGPOOL_VERSION:-"3.3.4"}
-PGPOOL_BRANCH=${PGPOOL_BRANCH:-"V3_3_STABLE"}
-POSTGRESQL_VERSION=9.2
-POSTGRESQL_VERSION2=92
+
 cd /var/lib/pgsql
 mkdir rpmbuild
 cd rpmbuild
@@ -30,5 +27,6 @@ cd ..
 diff -crN pgpool-II-$PGPOOL_VERSION head/pgpool-II-$PGPOOL_VERSION > pgpool-II-head.patch
 cd /var/lib/pgsql
 cp /tmp/pgpool-II-head.patch rpmbuild/SOURCES
+cp /dev/null rpmbuild/SOURCES/pgpool-II-head.patch
 cd rpmbuild/SPECS
-rpmbuild -ba pgpool.spec --define="pgpool_version $PGPOOL_VERSION" --define="pg_version $POSTGRESQL_VERSION2" --define="pghome /usr/pgsql-$POSTGRESQL_VERSION" --define="dist .pgdg"
+rpmbuild -ba pgpool.spec --define="pgpool_version $PGPOOL_VERSION" --define="pg_version $POSTGRESQL_VERSION2" --define="pghome /usr/pgsql-$POSTGRESQL_VERSION" --define="dist .rhel6"
--- CentOS6/pgpool-II-33-pg93/rpmbuild.sh
+++ CentOS6/pgpool-II-33-pg93/rpmbuild.sh
@@ -1,10 +1,7 @@
 #! /bin/sh
 # Script to create pgpool-II and PostgreSQL RPMs
 # Run this script as user "postgres".
-PGPOOL_VERSION=${PGPOOL_VERSION:-"3.3.4"}
-PGPOOL_BRANCH=${PGPOOL_BRANCH:-"V3_3_STABLE"}
-POSTGRESQL_VERSION=9.3
-POSTGRESQL_VERSION2=93
+
 cd /var/lib/pgsql
 mkdir rpmbuild
 cd rpmbuild
@@ -20,16 +17,16 @@ cp pgpool.spec ../rpmbuild/SPECS
 cp redhat/pgpool.conf.sample.patch ../rpmbuild/SOURCES
 cp redhat/pgpool.init ../rpmbuild/SOURCES
 cp redhat/pgpool.sysconfig ../rpmbuild/SOURCES
-#./configure --with-pgsql=/usr/pgsql-$POSTGRESQL_VERSION
-#make dist
-#mkdir /tmp/head
-#cp pgpool-II-$PGPOOL_VERSION.tar.gz /tmp/head
-#cd /tmp/head
-#tar xfz pgpool-II-$PGPOOL_VERSION.tar.gz
-#cd ..
-#diff -crN pgpool-II-$PGPOOL_VERSION head/pgpool-II-$PGPOOL_VERSION > pgpool-II-head.patch
+./configure --with-pgsql=/usr/pgsql-$POSTGRESQL_VERSION
+make dist
+mkdir /tmp/head
+cp pgpool-II-$PGPOOL_VERSION.tar.gz /tmp/head
+cd /tmp/head
+tar xfz pgpool-II-$PGPOOL_VERSION.tar.gz
+cd ..
+diff -crN pgpool-II-$PGPOOL_VERSION head/pgpool-II-$PGPOOL_VERSION > pgpool-II-head.patch
 cd /var/lib/pgsql
-#cp /tmp/pgpool-II-head.patch rpmbuild/SOURCES
+cp /tmp/pgpool-II-head.patch rpmbuild/SOURCES
 cp /dev/null rpmbuild/SOURCES/pgpool-II-head.patch
 cd rpmbuild/SPECS
-rpmbuild -ba pgpool.spec --define="pgpool_version $PGPOOL_VERSION" --define="pg_version $POSTGRESQL_VERSION2" --define="pghome /usr/pgsql-$POSTGRESQL_VERSION" --define="dist .pgdg"
+rpmbuild -ba pgpool.spec --define="pgpool_version $PGPOOL_VERSION" --define="pg_version $POSTGRESQL_VERSION2" --define="pghome /usr/pgsql-$POSTGRESQL_VERSION" --define="dist .rhel6"
