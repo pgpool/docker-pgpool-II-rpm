724fe68566d2fa8bf4e0b380f235011352ba2dc5
Tatsuo Ishii <ishii@postgresql.org>
2014-12-18 17:13:50 +0900

    Change rpm dist tag to rhel6.

--- CentOS6/pgpoolAdmin/rpmbuild.sh
+++ CentOS6/pgpoolAdmin/rpmbuild.sh
@@ -1,8 +1,6 @@
 #! /bin/sh
 # Script to create pgpoolAdmin RPMs
 # Run this script as user "postgres".
-POSTGRESQL_VERSION=9.3
-POSTGRESQL_VERSION2=93
 
 wget http://www.pgpool.net/download.php?f=pgpoolAdmin-$PGPOOL_ADMIN_VERSION.tar.gz -O /tmp/pgpoolAdmin-$PGPOOL_ADMIN_VERSION.tar.gz
 cd /tmp
@@ -14,4 +12,4 @@ mkdir BUILD BUILDROOT RPMS SOURCES SPECS SRPMS
 cp /tmp/pgpoolAdmin-$PGPOOL_ADMIN_VERSION.tar.gz SOURCES
 cp /tmp/pgpoolAdmin-$PGPOOL_ADMIN_VERSION/pgpoolAdmin.spec /var/lib/pgsql/rpmbuild/SPECS
 cd /var/lib/pgsql/rpmbuild/SPECS
-rpmbuild -ba pgpoolAdmin.spec --define="dist .pgdg" --define="pgpooladmin_version $PGPOOL_ADMIN_VERSION"
+rpmbuild -ba pgpoolAdmin.spec --define="dist .rhel6" --define="pgpooladmin_version $PGPOOL_ADMIN_VERSION"
