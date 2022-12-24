de3e4965edbe5cae3bf7d1ed89f42fe710645661
Bo Peng <pengbo@sraoss.co.jp>
2022-12-24 23:31:12 +0900

    Fix Rocky Linux9 installing error.

--- Rocky9/lib/build.sh
+++ Rocky9/lib/build.sh
@@ -7,7 +7,6 @@ export POSTGRESQL_VERSION2=`echo $POSTGRESQL_VERSION|sed s/\\\\.//`
 export POSTGRESQL_VERSION3=`echo "scale=0;$POSTGRESQL_VERSION*10" | bc | cut -d '.' -f 1`
 
 dnf install -y https://download.postgresql.org/pub/repos/yum/reporpms/EL-9-x86_64/pgdg-redhat-repo-latest.noarch.rpm
-dnf -qy module disable postgresql
 dnf install -y postgresql$POSTGRESQL_VERSION2-server postgresql$POSTGRESQL_VERSION2-devel
 
 su postgres < /tmp/rpmbuild.sh
