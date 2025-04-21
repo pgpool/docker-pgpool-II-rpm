cba05efaf6bd45d574c6d73f9187dd02d2a177d9
Bo Peng <pengbo@sraoss.co.jp>
2025-04-21 12:44:38 +0900

    Add support for Pgpool-II 4.6.

--- CentOS8/pgpool-II-release40/build.sh
+++
@@ -1,3 +0,0 @@
-su rpmuser < /tmp/rpmbuild.sh
-cp -rp /tmp/rpmbuild/RPMS /var/volum
-cp -rp /tmp/rpmbuild/SRPMS /var/volum
--- CentOS8/pgpoolAdmin/build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-su postgres < /tmp/rpmbuild.sh
-cp -rp /var/lib/pgsql/rpmbuild/RPMS /var/volum
-cp -rp /var/lib/pgsql/rpmbuild/SRPMS /var/volum
